568cfe552efae02405252c01bd67f9a3348d2ebe
Mahnoor Iqbal <mahnoor.iqbal456@gmail.com>
2024-01-28 15:50:08 +0500

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,4 +1,4 @@
-print("Area of a Triangle=A\n A=1/2 bxh\n where,\n b=Base of triangle\n h=Height of triangle\n Put the values in required field")
+print("Area of a Triangle=A\n As we know,\n A=1/2 bxh\n where,\n b=Base of triangle\n h=Height of triangle\n to calculate 'A' ,Put the values in required field")
 b=int(input("Base of triangle=b="))
 h=int(input("Height of triangle=h="))
 print("A=Area of Triangle=",(b)*(h)/2,"cm2")
